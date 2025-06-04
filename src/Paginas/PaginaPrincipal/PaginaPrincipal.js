59dc219d0bfd11d18f7641f4d24854df687af0df
Inmedia 11 <inmedia.pc.11@gmail.com>
2025-06-04 10:32:58 -0500

    0.0.8

--- src/Paginas/PaginaPrincipal/PaginaPrincipal.js
+++ src/Paginas/PaginaPrincipal/PaginaPrincipal.js
@@ -6,7 +6,7 @@ import Slider from './Componentes/Slider/Slider';
 import Categorias from './Componentes/Categorias/Categorias';
 import SoloPorHoras from './Componentes/SoloPorHoras/SoloPorHoras';
 import UltimasNovedades from './Componentes/UltimasNovedades/UltimasNovedades';
-import Ofertas from './Componentes/Ofertas/Ofertas';
+// import Ofertas from './Componentes/Ofertas/Ofertas';
 import SobreNosotros from './Componentes/SobreNosotros/SobreNosotros';
 import Distribuidores from '../../Componentes/Distribuidores/Distribuidores';
 import ModalDatos from './Componentes/ModalDatos/ModalDatos';
@@ -44,7 +44,7 @@ function PaginaPrincipal(){
 
                 <UltimasNovedades/>
                 
-                <Ofertas/>
+                {/* <Ofertas/> */}
 
                 <SobreNosotros/>
 
